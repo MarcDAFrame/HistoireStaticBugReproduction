819e36a8d7105d37603f06d75b87604ec01501fe
Marc Frame <mframe45@gmail.com>
2022-10-19 10:28:53 -0400

    failed now won't work in Histoire, but will work in svelte

--- src/lib/NoCases/Failed.svelte
+++ src/lib/NoCases/Failed.svelte
@@ -5,6 +5,6 @@
 </script>
 <NoCases 
 title="Failed" 
-src="/static/res/nocases/failed.svg" 
+src="/res/nocases/failed.svg" 
 subtitle="If there is an issue with your uploaded videos for a case, it will be shown here"
 />
